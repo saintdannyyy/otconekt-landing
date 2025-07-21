b8d1637cd0e10ac8f1b05a383bda644227b69bc1
Saintdannyyy <danieltesla746@gmail.com>
2025-07-21 00:55:56 +0000

    Update metadata with accurate description and generator

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  description: 'Ghanas No.1 Occupational TeleHealth Platform',
+  generator: 'OTConekt',
 }
 
 export default function RootLayout({
